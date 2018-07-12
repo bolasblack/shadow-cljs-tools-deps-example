38f8f492b9768012bab0408b881f876a6e95b8cc
c4605 <bolasblack@gmail.com>
2018-07-12 21:48:43 +0800

    upgrade shadow-cljs

--- src/test/core.cljs
+++ src/test/core.cljs
@@ -1,4 +1,21 @@
-(ns test.core)
+(ns test.core
+  (:require [cljs.spec.alpha :as s]))
 
-(defn plus [& args]
-  (apply + args))
+(defn test-fn [cb]
+  (when-not (s/valid? ::test-args-cb cb)
+    (throw (js/Error. (s/explain-str ::test-args-cb cb))))
+  (cb 1))
+
+(s/def ::test-args-cb
+  (s/fspec :args (s/tuple number?)
+           :ret number?))
+
+(s/def ::test-args
+  (s/tuple ::test-args-cb))
+
+(s/def ::test-result
+  number?)
+
+(s/fdef test-fn
+  :args ::test-args
+  :ret ::test-result)
--- test/test/core_test.cljs
+++ test/test/core_test.cljs
@@ -1,7 +1,11 @@
 (ns test.core-test
-  (:require [pjstadig.humane-test-output]
-            [cljs.test :as ct :refer [deftest testing is]]
-            [test.core :as c]))
+  (:require
+   [clojure.test.check]
+   [clojure.test.check.generators]
+   [clojure.test.check.properties]
+   [pjstadig.humane-test-output]
+   [cljs.test :as ct :refer [deftest testing is]]
+   [test.core :as c]))
 
 (deftest hello
-  (is (c/plus 1 1)))
+  (is (c/test-fn identity) 1))
